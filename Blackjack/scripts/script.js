56d27ae210a505e71bc922f586ab9504bc128716
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-11-11 21:06:42 -0600

    Add files via upload

--- Blackjack/scripts/script.js
+++ Blackjack/scripts/script.js
@@ -1,5 +1,8 @@
 const score = document.getElementById("scoreEl");
 const dealerCards = document.getElementById("dealersHand");
+const hitBtn = document.getElementById("hitBtn");
+const stayBtn = document.getElementById("stayBtn");
+const doubleBtn = document.getElementById("doubleBtn")
 
 // Create playerTotal (intially set to 1000 and put in DOM)
 let playerTotal = 1000;
@@ -7,10 +10,9 @@ score.textContent = `Cash: $${playerTotal}`;
 
 // Create deck
 let suits = ["spades", "diamonds", "clubs", "hearts"];
-let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
+let card = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] ;
 let deck = new Array();
-
 for (let i  = 0; i < suits.length; i++)
 {
     for (let x = 0; x < values.length; x++)
@@ -19,5 +21,23 @@ for (let i  = 0; i < suits.length; i++)
         deck.push(card);
     }
 }
+
+
+// Draw cards
+function drawCard() {
+    let randomNum = Math.floor(Math.random() * 52);
+    let cardValue = deck[randomNum];
+    return cardValue;
+}
+
+let playerFirstCard = drawCard();
+let playerSecondCard = drawCard();
+let playerHandTotal = playerFirstCard.Value + playerSecondCard.Value;
+
+let dealerFirstCard = drawCard();
+let dealerSecondCard = drawCard();
+let dealerHandTotal = dealerFirstCard.Value + dealerSecondCard.Value;
+
+
 
 
